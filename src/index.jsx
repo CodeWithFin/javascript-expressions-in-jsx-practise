e2d5b2c438181bd433e14ae08372e39ffa0134c6
Finley <finley.mwachia12@gmail.com>
2024-08-08 13:52:12 +0300

    add files required for the project

---
+++ src/index.jsx
@@ -0,0 +1,14 @@
+//Create a react app from scratch.
+//It should display 2 paragraph HTML elements.
+//The paragraphs should say:
+//Created by YOURNAME.
+//Copyright CURRENTYEAR.
+//E.g.
+//Created by Angela Yu.
+//Copyright 2019.
+
+// If you're running this locally in VS Code use the commands:
+// npm install
+// to install the node modules and
+// npm run dev
+// to launch your react project in your browser
